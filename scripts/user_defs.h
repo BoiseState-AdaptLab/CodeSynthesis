224b391a4735b70fb60dd64529c77722575e2d90
Tpops <eyepud@gmail.com>
2022-03-28 17:11:52 -0600

    Added include headers for coo to csr, coo to mcoo

---
+++ scripts/user_defs.h
@@ -0,0 +1,22 @@
+#ifndef MORTON_BITS_HPP
+#define MORTON_BITS_HPP
+#include <cstdint>
+
+#define MORTON(a,b) encode(a,b)
+// Compute the 2d Morton code for a pair of indices
+inline uint64_t encode(const uint32_t x, const uint32_t y) {
+    return split(x) | split(y) << 1;
+}
+
+
+inline uint64_t split(const uint32_t a) {
+    uint64_t x = a;
+    x = (x | x << 16) & 0x0000ffff0000ffffUL;
+    x = (x | x <<  8) & 0x00ff00ff00ff00ffUL;
+    x = (x | x <<  4) & 0x0f0f0f0f0f0f0f0fUL;
+    x = (x | x <<  2) & 0x3333333333333333UL;
+    x = (x | x <<  1) & 0x5555555555555555UL;
+    return x;
+}
+#endif
+
