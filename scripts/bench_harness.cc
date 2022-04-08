5fe8304819a384dd07b16b555c87edfd84a2f8a4
AaronStGeorge <aaronstgeorge@gmail.com>
2022-04-08 09:35:57 -0600

    More logging

--- scripts/bench_harness.cc
+++ scripts/bench_harness.cc
@@ -137,21 +137,24 @@ public:
 
 std::pair<CSR *, uint64_t> COOToCSR(uint64_t nnz, uint64_t rank,
                                     const std::vector<uint64_t> &dims,
-                                    std::vector<double> &cooValues,
-                                    std::vector<std::vector<uint64_t>> &coord) {
+                                    const std::vector<double> &cooValues,
+                                    const std::vector<std::vector<uint64_t>> &coord) {
     int nr = dims[0];
     int nc = dims[1];
 
     CSR *csr = new CSR(nr, nnz);
+    std::vector<int> &col = csr->col;
+    std::vector<int> &rowptr = csr->rowptr;
+    std::vector<double> &values = csr->values;
 
     auto start = std::chrono::high_resolution_clock::now();
 
 #define EX_ROW1(n) coord[0][n]
 #define EX_COL1(n) coord[1][n]
 #define EX_ACOO(n) cooValues[n]
-#define EX_COL2(n) csr->col[n]
-#define EX_ROWPTR(n) csr->rowptr[n]
-#define EX_ACSR(n) csr->values[n]
+#define EX_COL2(n) col[n]
+#define EX_ROWPTR(n) rowptr[n]
+#define EX_ACSR(n) values[n]
 #define NR nr
 #define NC nc
 #define NNZ nnz
@@ -176,7 +179,7 @@ std::pair<CSR *, uint64_t> COOToCSR(uint64_t nnz, uint64_t rank,
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename of matrix market exchange file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <filename (should be in matrix market exchange format)>\n", argv[0]);
         exit(1);
     }
     char *filename = argv[1];
@@ -201,6 +204,7 @@ int main(int argc, char *argv[]) {
     std::vector<double> values(nnz);
 
     // Read file into vectors
+    printf("reading %s into vectors\n", filename);
     for (uint64_t k = 0; k < nnz; k++) {
         if (!fgets(line, kColWidth, file)) {
             fprintf(stderr, "Cannot find next line of data in %s\n", filename);
@@ -216,13 +220,13 @@ int main(int argc, char *argv[]) {
         values[k] = value;
     }
 
+    printf("converting %s into csr\n", filename);
     auto p = COOToCSR(nnz, rank, dims, values, coord);
     CSR *csr = p.first;
     uint64_t microseconds = p.second;
 
+    printf("verifying %s conversion\n", filename);
     auto checkCSR = [&csr, &dims](const std::vector<uint64_t> &cord) -> double {
-        // TODO: check dim
-
         uint64_t inI = cord[0];
         uint64_t inJ = cord[1];
         uint64_t nr = dims[0];
@@ -238,11 +242,8 @@ int main(int argc, char *argv[]) {
     };
 
     if (verify(coord, values, checkCSR)) {
-        printf("[PASS] coo->csr %s, time: %lu μs, \n", filename, microseconds);
-        return 0;
+        printf("[PASS] coo->csr %s, time: %lu microseconds, \n", filename, microseconds);
     } else {
-        printf("[FAIL] coo->csr %s, time: %lu μs, \n", filename, microseconds);
-        return 1;
+        printf("[FAIL] coo->csr %s, time: %lu microseconds, \n", filename, microseconds);
     }
 }
-
