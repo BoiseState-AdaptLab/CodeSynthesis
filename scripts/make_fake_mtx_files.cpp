ebe4196a12876574f8ada659768daa933be1a01b
AaronStGeorge <aaronstgeorge@gmail.com>
2022-04-09 10:57:03 -0600

    Approximate time

--- scripts/make_fake_mtx_files.cpp
+++ scripts/make_fake_mtx_files.cpp
@@ -1,30 +1,39 @@
+#include <climits>
 #include <cstdio>
 #include <cstdlib>
+#include <filesystem>
 #include <sstream>
-#include <math.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main() {
-    for (int i = 1; i < 6; i++) {
-        int n = (int) pow(10, i);
+int main()
+{
+    mkdir("fake_data", 0777);
+    for (int n = 100; n <= 10000; n += 100)
+    {
         std::stringstream ss;
-        ss << "/home/aaron/Dev/c++/CodeSynthesis/test/data/fake_nnz_" << n << ".mtx";
+        ss << "./fake_data/fake_nnz_" << n << ".mtx";
         const std::string tmp = ss.str();
         const char *filename = tmp.c_str();
+
         FILE *file = fopen(filename, "w");
-        if (!file) {
+        if (!file)
+        {
             fprintf(stderr, "Cannot find %s\n", filename);
             exit(1);
         }
 
         fprintf(file, "%%%%MatrixMarket matrix coordinate real general\n");
         fprintf(file, "%d %d %d\n", n / 2, n / 2, n);
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++)
+        {
             int row = (j / 2) + 1;
             int col = row + (j % 2);
-            if (col == n / 2 + 1) {
+            if (col == n / 2 + 1)
+            {
                 col -= 2;
             }
-            fprintf(file, "%d %d %f \n", row, col, row + (double) col / (double) n);
+            fprintf(file, "%d %d %f \n", row, col, row + (double)col / (double)n);
         }
 
         fclose(file);
