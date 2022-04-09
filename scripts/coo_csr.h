f5ef9539ae9f7e7c36bb5fbb203b7681bc0ec748
AaronStGeorge <aaronstgeorge@gmail.com>
2022-04-09 11:18:29 -0600

    Comment out prints (for now)

--- scripts/coo_csr.h
+++ scripts/coo_csr.h
@@ -85,7 +85,7 @@ return false;
 
 if (NC >= 1 && NR >= 1) {
   
-   std::cerr << "S0: Permute Code Start\n";	
+//   std::cerr << "S0: Permute Code Start\n";
    for(t2 = 0; t2 <= NNZ-1; t2++) {
     if (col1_1(t1,t2) >= 0 && NC >= col1_1(t1,t2)+1) {
       t4=row1_0(t1,t2);
@@ -93,7 +93,7 @@ if (NC >= 1 && NR >= 1) {
       s0(0,t2,0,t4,0,t6,0);
     }
   }
-  std::cerr << "S0: Permute Code Ended\n";
+//  std::cerr << "S0: Permute Code Ended\n";
   for(t2 = 0; t2 <= NNZ-1; t2++) {
     if (col1_1(t1,t2) >= 0 && NC >= col1_1(t1,t2)+1) {
       t4=row1_0(t1,t2);
@@ -101,7 +101,7 @@ if (NC >= 1 && NR >= 1) {
       s1(1,t2,0,t4,0,t6,0);
     }
   }
-  std::cerr << "S1: completed \n";
+//  std::cerr << "S1: completed \n";
   for(t2 = 0; t2 <= NNZ-1; t2++) {
     if (row1_0(t1,t2) >= 0 && NC >= col1_1(t1,t2)+1 && NR >= row1_0(t1,t2)+1 && col1_1(t1,t2) >= 0) {
       t3=row1_0(t1,t2);
@@ -111,7 +111,7 @@ if (NC >= 1 && NR >= 1) {
       }
     }
   }
-  std::cerr << "S2: completed \n";
+//  std::cerr << "S2: completed \n";
   for(t2 = 0; t2 <= NNZ-1; t2++) {
     if (col1_1(t1,t2) >= 0 && NR >= row1_0(t1,t2)+1 && NC >= col1_1(t1,t2)+1 && row1_0(t1,t2) >= 0) {
       t3=row1_0(t1,t2);
@@ -123,7 +123,7 @@ if (NC >= 1 && NR >= 1) {
       }
     }
   }
-  std::cerr << "S3: completed \n";
+//  std::cerr << "S3: completed \n";
   for(t2 = 0; t2 <= NNZ-1; t2++) {
     if (row1_0(t1,t2) >= 0 && NC >= col1_1(t1,t2)+1 && NR >= row1_0(t1,t2)+1 && col1_1(t1,t2) >= 0) {
       t3=row1_0(t1,t2);
@@ -133,7 +133,7 @@ if (NC >= 1 && NR >= 1) {
       }
     }
   }
-  std::cerr << "S4: completed \n";
+//  std::cerr << "S4: completed \n";
   for(t2 = 0; t2 <= NNZ-1; t2++) {
     if (row1_0(t1,t2) >= 0 && NC >= col1_1(t1,t2)+1 && NR >= row1_0(t1,t2)+1 && col1_1(t1,t2) >= 0) {
       t3=row1_0(t1,t2);
@@ -144,13 +144,13 @@ if (NC >= 1 && NR >= 1) {
     }
   }
 }
-  std::cerr << "S5: completed \n";
+//  std::cerr << "S5: completed \n";
 for(t2 = 0; t2 <= NR-1; t2++) {
   for(t4 = t2+1; t4 <= NR; t4++) {
     s6(6,t2,0,t4,0,0,0);
   }
 }
-  std::cerr << "S6: completed \n";
+//  std::cerr << "S6: completed \n";
 if (NR >= 1 && NC >= 1) {
   for(t2 = 0; t2 <= NNZ-1; t2++) {
     if (row1_0(t1,t2) >= 0 && NC >= col1_1(t1,t2)+1 && col1_1(t1,t2) >= 0 && NR >= row1_0(t1,t2)+1) {
@@ -169,7 +169,7 @@ if (NR >= 1 && NC >= 1) {
   }
 }
 
-  std::cerr << "S7: Copy code completed \n";
+//  std::cerr << "S7: Copy code completed \n";
 
 
 
