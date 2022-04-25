e5c5eb5889ff544e7cac3ae9045b248c2d00e611
Tpops <eyepud@gmail.com>
2022-04-25 09:15:18 -0600

    files

---
+++ scripts/coo_mcoo_opt.h
@@ -0,0 +1,93 @@
+#include <synth.h> 
+#define min(a,b) a < b ? a : b
+#define max(a,b) a > b ? a: b
+auto P0Comp = [](const std::vector<int>& a, const std::vector<int>& b){
+if(-MORTON(a[0], a[1]) + MORTON(b[0], b[1]) - 1>= 0)
+return true;
+if(-MORTON(a[0], a[1]) + MORTON(b[0], b[1]) - 1>= 0)
+return true;
+    return false;
+};
+Permutation<int,decltype(P0Comp)> * P0 = new Permutation <int,decltype(P0Comp)>(P0Comp);
+#define ACOO(n) EX_ACOO(n)
+//TODO n generated for RHS, fix!!!
+#define AMCOO(n1) EX_AMCOO(n1)
+#undef s0
+#undef s_0
+#undef s1
+#undef s_1
+#undef s2
+#undef s_2
+#undef s3
+#undef s_3
+#define s_0(n, tv1, tv2)   P0->insert({row1(n), col1(n)}) 
+#define s0(__x0, a1, __x2, a3, __x4, a5, __x6)   s_0(a1, a3, a5);
+#define s_1(n)   col3(P0(row1(n), col1(n)))=col1(n) 
+#define s1(__x0, a1, tv2, tv3, __x4, __x5, __x6, __x7, __x8)   s_1(a1);
+#define s_2(n)   row3(P0(row1(n), col1(n)))=row1(n) 
+#define s2(__x0, a1, tv2, tv3, __x4, __x5, __x6, __x7, __x8)   s_2(a1);
+#define s_3(n, n1)   AMCOO(n1) = ACOO(n ) 
+#define s3(__x0, a1, tv2, tv3, __x4, a3, __x6, __x7, __x8)   s_3(a1, a3);
+
+#undef P0_2
+#undef col1_1
+#undef col3_3
+#undef row1_0
+#undef row3_4
+#define P0(t0,t1) P0->get({t0,t1})
+#define P0_2(__tv0, __tv1, __tv2, __tv3) P0(__tv2, __tv3)
+#define col1(t0) EX_COL1(t0)
+#define col1_1(__tv0, __tv1) col1(__tv1)
+#define col3(t0) EX_COL3(t0)
+#define col3_3(__tv0, __tv1, __tv2, __tv3, __tv4, __tv5) col3(__tv5)
+#define row1(t0) EX_ROW1(t0)
+#define row1_0(__tv0, __tv1) row1(__tv1)
+#define row3(t0) EX_ROW3(t0)
+#define row3_4(__tv0, __tv1, __tv2, __tv3, __tv4, __tv5) row3(__tv5)
+
+int t1 = 3; 
+int t2 = 0; 
+int t3 = 0; 
+int t4 = 0; 
+int t5 = 0; 
+int t6 = 0; 
+int t7 = 0; 
+int t8 = 0; 
+int t9 = 0; 
+
+if (NR >= 1 && NC >= 1) {
+  for(t2 = 0; t2 <= NNZ-1; t2++) {
+    if (row1_0(t1,t2) >= 0 && NC >= col1_1(t1,t2)+1 && NR >= row1_0(t1,t2)+1 && col1_1(t1,t2) >= 0) {
+      t4=row1_0(t1,t2);
+      t6=col1_1(t1,t2);
+      s0(0,t2,0,t4,0,t6,0);
+    }
+  }
+  for(t2 = 0; t2 <= NNZ-1; t2++) {
+    if (NC >= col1_1(t1,t2)+1 && NR >= row1_0(t1,t2)+1 && col1_1(t1,t2) >= 0 && row1_0(t1,t2) >= 0) {
+      t3=row1_0(t1,t2);
+      t4=col1_1(t1,t2);
+      s1(1,t2,t3,t4,0,0,0,0,0);
+      s2(2,t2,t3,t4,0,0,0,0,0);
+      t6=P0_2(t1,t2,t3,t4);
+      if (row3_4(t1,t2,t3,t4,t5,t6) == t3 && col3_3(t1,t2,t3,t4,t5,t6) == col1_1(t1,t2)) {
+          s3(3,t2,t3,t4,0,t6,0,0,0);
+      }
+    }
+  }
+}
+
+#undef s0
+#undef s_0
+#undef s1
+#undef s_1
+#undef s2
+#undef s_2
+#undef s3
+#undef s_3
+#undef P0_2
+#undef col1_1
+#undef col3_3
+#undef row1_0
+#undef row3_4
+delete P0; 
---
+++ scripts/csr_csc_opt.h
@@ -0,0 +1,136 @@
+#include <synth.h> 
+// Optimizations
+// 1. Redundant Statement Elimination
+// 2. Fusion
+// 3. 
+
+#define min(a,b) a < b ? a : b
+#define max(a,b) a > b ? a: b
+Permutation<int> * P0 = new Permutation<int>(2);
+auto P1Comp = []( const std::vector<int>& a, const std::vector<int>& b){
+if (a[1] < b[1] )    return true;
+// Override set equality behaviour (Tuowen)
+return a < b;
+};
+Permutation<int,decltype(P1Comp)>* P1 = new Permutation <int,decltype(P1Comp)>(P1Comp);
+#define ACSR(i,k) EX_ACSR(k)
+#define ACSC(j,k) EX_ACSC(k)
+#undef s0
+#undef s_0
+#undef s1
+#undef s_1
+#undef s2
+#undef s_2
+#undef s3
+#undef s_3
+#undef s4
+#undef s_4
+#undef s6
+#undef s_6
+#undef s7
+#undef s_7
+#define s_0(i, k)   P0->insert({i, col2(k),col2(k)}) 
+#define s0(__x0, a1, __x2, a3, __x4, __x5, __x6, __x7, __x8)   s_0(a1, a3);
+#define s_1(i, k, tv2, tv3)   P1->insert({i, col2(k)}) 
+#define s1(__x0, a1, __x2, a3, __x4, a5, __x6, a7, __x8)   s_1(a1, a3, a5, a7);
+#define s_2(i, k)   colptr(col2(k)) = min(colptr(col2(k)),P1(i, col2(k))) 
+#define s2(__x0, a1, __x2, a3, tv4, __x5, __x6, __x7, __x8, __x9)   s_2(a1, a3);
+#define s_3(i, k)   colptr(col2(k) + 1) = max(colptr(col2(k) + 1),P1(i, col2(k)) + 1) 
+#define s3(__x0, a1, __x2, a3, tv4, __x5, __x6, __x7, __x8, __x9)   s_3(a1, a3);
+#define s_4(i, k)   row4(P1(i, col2(k)))=i 
+#define s4(__x0, a1, __x2, a3, tv4, __x5, __x6, __x7, __x8, __x9)   s_4(a1, a3);
+#define s_5(i, k, j, k1)   row4(k1)=i 
+#define s5(__x0, a1, __x2, a3, tv4, __x5, a5, __x7, a7, __x9)   s_5(a1, a3, a5, a7);
+#define s_6(e1, e2)   if ( not (colptr(e1) <= colptr(e2))){colptr(e2) = colptr(e1);} 
+#define s6(__x0, a1, __x2, a3, __x4, __x5, __x6, __x7, __x8)   s_6(a1, a3);
+#define s_7(i, k, j, k1)   ACSC(j,k) = ACSR(i,k ) 
+#define s7(__x0, a1, __x2, a3, tv4, __x5, a5, __x7, a7, __x9)   s_7(a1, a3, a5, a7);
+
+#undef P1_4
+#undef col2_2
+#undef colptr_6
+#undef colptr_7
+#undef row4_5
+#undef P0_3
+#undef rowptr_0
+#undef rowptr_1
+#define P1(t0,t1) P1->get({t0,t1})
+#define P1_4(__tv0, __tv1, __tv2, __tv3, __tv4) P1(__tv1, __tv4)
+#define col2(t0) EX_COL(t0)
+#define col2_2(__tv0, __tv1, __tv2, __tv3) col2(__tv3)
+#define colptr(t0) EX_COLPTR(t0)
+#define colptr_6(__tv0, __tv1, __tv2, __tv3, __tv4, __tv5, __tv6) colptr(__tv6)
+#define colptr_7(__tv0, __tv1, __tv2, __tv3, __tv4, __tv5, __tv6) colptr(__tv6 + 1)
+#define row4(t0) EX_ROW(t0)
+#define row4_5(__tv0, __tv1, __tv2, __tv3, __tv4, __tv5, __tv6, __tv7, __tv8) row4(__tv8)
+#define P0(t0,t1) P0->get({t0,t1})
+#define P0_3(__tv0, __tv1, __tv2, __tv3, __tv4) P0(__tv1, __tv4)
+#define rowptr(t0) EX_ROWPTR(t0)
+#define rowptr_0(__tv0, __tv1) rowptr(__tv1)
+#define rowptr_1(__tv0, __tv1) rowptr(__tv1 + 1)
+
+int t1 = 7; 
+int t2 = 0; 
+int t3 = 0; 
+int t4 = 0; 
+int t5 = 0; 
+int t6 = 0; 
+int t7 = 0; 
+int t8 = 0; 
+int t9 = 0; 
+int t10 = 0; 
+
+if (NC >= 1) {
+  for(t2 = 0; t2 <= NR-1; t2++) {
+    for(t4 = rowptr_0(t1,t2); t4 <= rowptr_1(t1,t2)-1; t4++) {
+      t8=col2_2(t1,t2,t3,t4);
+      s1(1,t2,0,t4,0,t2,0,t8,0);
+    }
+  }
+  // Fused row, colptr and data copy
+  for(t2 = 0; t2 <= NR-1; t2++) {
+    for(t4 = rowptr_0(t1,t2); t4 <= rowptr_1(t1,t2)-1; t4++) {
+      if (NC >= col2_2(t1,t2,t3,t4)+1 && col2_2(t1,t2,t3,t4) >= 0) {
+        t5=col2_2(t1,t2,t3,t4);
+        t7=t5;
+        t9=P1_4(t1,t2,t3,t4,t5);
+        s3(3,t2,0,t4,t5,0,0,0,0,0);
+        s2(2,t2,0,t4,t5,0,0,0,0,0);
+        s7(7,t2,0,t4,t5,0,t7,0,t9,0);
+        s4(4,t2,0,t4,t5,0,0,0,0,0);
+      }
+    }
+  }
+}
+// Optimize UQ verification.
+for(t2 = 0; t2 <= NR-1; t2++) {
+   t4 = t2 + 1; 
+   s6(6,t2,0,t4,0,0,0,0,0);
+}
+
+#undef s0
+#undef s_0
+#undef s1
+#undef s_1
+#undef s2
+#undef s_2
+#undef s3
+#undef s_3
+#undef s4
+#undef s_4
+#undef s5
+#undef s_5
+#undef s6
+#undef s_6
+#undef s7
+#undef s_7
+#undef P1_4
+#undef col2_2
+#undef colptr_6
+#undef colptr_7
+#undef row4_5
+#undef P0_3
+#undef rowptr_0
+#undef rowptr_1
+delete P0; 
+delete P1; 
